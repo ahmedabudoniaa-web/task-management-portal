00cabeaa1635f851f0eef3a64998e4731e711fa7
ahmedabudoniaa-web <ahmed.abudoniaa@gmail.com>
2026-06-23 15:07:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 
 const link = document.createElement('link')
 link.rel = 'stylesheet'
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/%40tabler/icons-webfont/3.3.0/tabler-icons.min.css'
+link.href = 'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.3.0/dist/tabler-icons.min.css'
 document.head.appendChild(link)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
